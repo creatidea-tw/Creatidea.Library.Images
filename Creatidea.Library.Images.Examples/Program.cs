a2b11209ec41e7e7047f847d48925cb84b3e94dc
紙鈔魚 <abc12207@gmail.com>
2015-12-20 16:16:05 +0800

    add example project

---
+++ Creatidea.Library.Images.Examples/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creatidea.Library.Images.Examples
+{
+    /// <summary>
+    /// 測試範例.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
