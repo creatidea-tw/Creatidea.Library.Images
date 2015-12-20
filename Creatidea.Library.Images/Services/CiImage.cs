213b8dd6db53a8fd178d870bc7440abc168702d1
紙鈔魚 <abc12207@gmail.com>
2015-12-20 15:57:29 +0800

    Init.

---
+++ Creatidea.Library.Images/Services/CiImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creatidea.Library.Images.Services
+{
+    /// <summary>
+    /// CiImage 公開類別.
+    /// </summary>
+    public class CiImage
+    {
+    }
+}
